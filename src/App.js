e23c78584b39df9e5d95adf4056f040808c183bc
Rafael <rafael@inpay.pl>
2019-05-29 16:41:58 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Reduxtest from './components/reduxtest';
+import Test from './components/test';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <Reduxtest />
+        <Test />
+      </div>
+    );
+
+  }
 }
 
 export default App;
